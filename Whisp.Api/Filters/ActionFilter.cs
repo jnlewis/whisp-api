84fc1a04faa7404d3bd88e902995ff2b195cdb56
Jeffrey Lewis <novensiles@gmail.com>
2019-01-01 10:20:33 +0800

    Added global request logging

--- Whisp.Api/Filters/ActionFilter.cs
+++ Whisp.Api/Filters/ActionFilter.cs
@@ -1,18 +1,35 @@
 ﻿using System.Web.Http.Filters;
 using System.Web.Http.Controllers;
+using Whisp.Application.Interfaces;
+using Unity.Attributes;
 
 namespace Whisp.Api.Filters
 {
     public class ActionFilter : ActionFilterAttribute
     {
+        [Dependency]
+        public IAppLogger Logger { get; set; }
+        
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
+            LogRequest(actionContext);
             base.OnActionExecuting(actionContext);
         }
 
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             base.OnActionExecuted(actionExecutedContext);
+        }
+
+        private void LogRequest(HttpActionContext actionContext)
+        {
+            try
+            {
+                Logger.Trace($"Request: ({actionContext.Request.Method.ToString()}) {actionContext.Request.RequestUri.ToString()}");
+            }
+            catch
+            {
+            }
         }
     }
 }
